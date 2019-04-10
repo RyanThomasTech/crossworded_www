a56bf099ef137a74c220a33aa62869e51b333a2a
Ryan Thomas <contact@ryanthomas.tech>
2019-04-10 10:59:14 -0400

    some more style changes. There seems to be some other force acting on the Link styles. I can't change certain part of their styles, including the underline or their hover behavior. Might need to consult SO.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 import styles from "./navbar.module.css"
-import { rhythm } from "../utils/typography"
 
 class NavBar extends React.Component {
   render() {
     return (
-      <nav
-        style={{
-          display: "box",
-          margin: "auto",
-          maxWidth: rhythm(24),
-        }}
-      >
-        <ul class={styles.ul}>
+      <nav className={styles.nav}>
+        <ul>
           <li>
-            <Link to="/about">About</Link>
+            <Link style={{ textDecoration: 'none' }} to="/about">
+              About
+            </Link>
           </li>
           <li>
             <Link to="/about">About</Link>
