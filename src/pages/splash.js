19d47304870ecb64c603514ee4dfa21248567d29
Ryan Thomas <contact@ryanthomas.tech>
2019-05-10 00:51:34 -0400

    issue: the workaround for revealing all letters regardless of which one was hovered also causes all letters in other crosswords to be revealed at the same time. Also, not liking how these words look when arrayed vertically anyway. Fonts were not made to be vertical.

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -4,45 +4,63 @@ import { jsx, css } from "@emotion/core"
 import Layout from "../components/layout"
 import "./splash.css"
 
+/* TODO: javascript decorations
+ * first priority is getting a css-primary 
+ * website out there in case of NoScript users,
+ * will be concerned with javascript decos later
+ *
+function mouseoverBrie() {
+  let thingie = document.getElementsByClassName("brie")[0];
+  thingie.style.color = "red";
+}
+
+*/
+
 class splash extends React.Component {
   render() {
     return (
       <Layout location="splash">
-          <div class="container">
-              <span class="brie-b brie"><h1>B</h1></span>
-              <span class="brie-i brie"><h1>I</h1></span>
-              <span class="brie-e brie"><h1>E</h1></span>
-          <span class="ryan-r brie">
+        <span className="container">
+          <span className="ryan-r prog">
             <h1>R</h1>
           </span>
-          <span class="ryan-y">
+          <span className="ryan-y">
             <h1>y</h1>
           </span>
-          <span class="ryan-a0">
+          <span className="ryan-a0 artist">
             <h1>a</h1>
           </span>
-          <span class="ryan-n">
+          <span className="ryan-n">
             <h1>n</h1>
           </span>
-          <span class="ryan-t">
+          <span className="ryan-t">
             <h1>T</h1>
           </span>
-          <span class="ryan-h">
+          <span className="ryan-h">
             <h1>h</h1>
           </span>
-          <span class="ryan-o">
-              <h1>o</h1>
+          <span className="ryan-o">
+            <h1>o</h1>
           </span>
-          <span class="ryan-m">
+          <span className="ryan-m">
             <h1>m</h1>
           </span>
-          <span class="ryan-a1">
+          <span className="ryan-a1">
             <h1>a</h1>
           </span>
-          <span class="ryan-s">
+          <span className="ryan-s">
             <h1>s</h1>
           </span>
-        </div>
+          <span className="prog-before prog crossword">
+            <h2>prog</h2>
+          </span>
+          <span className="prog-after prog crossword">
+            <h2>ammer</h2>
+        </span>
+        <span className="artist-after artist crossword">
+            <h2>rtist</h2>
+        </span>
+        </span>
       </Layout>
     )
   }
